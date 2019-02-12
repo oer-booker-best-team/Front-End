59d3d9479651e31f041382073a81bb5f58d2abb7
Leila <nb.leila10@gmail.com>
2019-02-12 17:27:31 -0600

    Initial Commit: Setup app with folder structure and installs

---
+++ oer-bookr/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to OER Bookr!!!</h1>
+        {/* <HomePage /> */}
+      </div>
+    )
+  }
+}
+
+export default App
