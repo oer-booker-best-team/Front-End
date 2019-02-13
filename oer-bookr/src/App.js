42f5c5add453a85d168f42460c03f0ffa41b1898
Leila <nb.leila10@gmail.com>
2019-02-12 18:13:05 -0600

    Add authentication
    Add HOC authentication to check if there's a logged in user (saved in localstorage)
    If no logged in user, show login form
    When click login button, save username in localstorage

--- oer-bookr/src/App.js
+++ oer-bookr/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import Authenticate from "./hoc/Authenticate"
 
 class App extends Component {
   render() {
@@ -11,4 +12,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default Authenticate(App)
